5b2f137259cc58107d7bf5ba5ba6b0624fe9c574
Alexander Morozov <iceberginaction@gmail.com>
2024-07-08 10:36:33 +0400

    Initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-html-entities'), 'version' => '4a426a1c68c1092cda6b');
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { decodeEntities } from '@wordpress/html-entities';
+
+
+const { registerPaymentMethod } = window.wc.wcBlocksRegistry;
+const { getSetting } = window.wc.wcSettings;
+
+const settings = getSetting('misha_data', {});
+
+const label = decodeEntities(settings.title);
+
+const Content = () => {
+    return decodeEntities( settings.description || '' )
+};
+
+const Label = (props) => {
+    const { PaymentMethodLabel } = props.components
+    return <PaymentMethodLabel text={ label } />
+};
+
+registerPaymentMethod({
+    name: 'misha',
+    label: <Label />,
+    content: <Content />,
+    edit: <Content />,
+    canMakePayment: () => true,
+    ariaLabel: label,
+    supports: {
+        features: settings.supports,
+    },
+});
